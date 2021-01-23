cd2a6fc7445f83626f19e56365e9055d7d04bc21
Yona <uzicandi@gmail.com>
2021-01-23 22:51:17 +0900

    action create function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import { createStore } from 'redux';
+import { unlink } from 'fs';
 
 const form = document.querySelector('form');
 const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
+
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
+
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+};
 
 const reducer = (state = [], action) => {
   console.log(action);
@@ -21,11 +37,37 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchDeleteToDo = e => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDo(id));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = '';
+  toDos.forEach(toDo => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = 'DELETE';
+    btn.addEventListener('click', dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
+
+const dispatchAddToDo = text => {
+  store.dispatch(addToDo(text));
+};
+
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener('submit', onSubmit);
