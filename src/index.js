727f960068eefa864ab063e959aecfe24b67bb21
Yona <uzicandi@gmail.com>
2021-01-23 21:12:43 +0900

    variable declare & switch case

--- src/index.js
+++ src/index.js
@@ -8,15 +8,28 @@ const number = document.querySelector('span');
 
 number.innerText = 0;
 
+// 오타방지를 위한 변수 선언
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
 const countModifier = (count = 0, action) => {
-  // console.log(count, action);
-  if (action.type === 'ADD') {
-    return count + 1;
-  } else if (action.type === 'MINUS') {
-    return count - 1;
-  } else {
-    return count;
+  // if (action.type === 'ADD') {
+  //   return count + 1;
+  // } else if (action.type === 'MINUS') {
+  //   return count - 1;
+  // } else {
+  //   return count;
+  // }
+
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
+
   /**
    * = reducer
    * 1. 데이터를 바꿔줌 (modify state)
@@ -33,11 +46,11 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: 'ADD' });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: 'MINUS' });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener('click', handleAdd);
