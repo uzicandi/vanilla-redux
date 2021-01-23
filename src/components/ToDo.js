cbc1aad615f605e45bb4b955c2e9bcdd648c2813
Yona <uzicandi@gmail.com>
2021-01-24 01:53:53 +0900

    delete dispatch

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { actionCreators } from '../store';
 
-function ToDo({ text }) {
+function ToDo({ text, onBtnClick }) {
   return (
     <li>
-      {text} <button>DEL</button>
+      {text} <button onClick={onBtnClick}>DEL</button>
     </li>
   );
 }
 
-export default ToDo;
+// ToDo 컴포넌트에 onBtnClick 넣을 수 있게됨
+function mapDispatchToProps(dispatch, ownProps) {
+  // ownProps (component props받기))
+  return {
+    onBtnClick: () => dispatch(actionCreators.deleteTodo(ownProps.id))
+  };
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ToDo);
