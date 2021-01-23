5bf55a9da64a4ad05fe087cc1aea0ee38bd7c9e6
Yona <uzicandi@gmail.com>
2021-01-23 21:02:58 +0900

    action, subscribe

--- src/index.js
+++ src/index.js
@@ -6,15 +6,39 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const countModifier = (state = 0) => {
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
+  // console.log(count, action);
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  } else {
+    return count;
+  }
   /**
    * = reducer
    * 1. 데이터를 바꿔줌 (modify state)
    * 2. return하는 것은 우리 어플리케이션의 데이터임
    * 3. 데이터를 바꿀 수 있는 유일한 함수 => 데이터가 한 곳에 있게 됨
    */
-  return state;
 };
 const countStore = createStore(countModifier); // createStore는 Reducer가 필요함
 
-console.log(countStore.getState()); // hello (reducer return)
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: 'ADD' });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: 'MINUS' });
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
