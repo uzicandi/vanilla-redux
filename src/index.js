1c6277d466448cef3624426b77386308a19bf0bf
Yona <uzicandi@gmail.com>
2021-01-23 18:21:49 +0900

    state, reducer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
+import { createStore } from 'redux';
+// store = data를 넣는곳 = state 넣는곳을 생성함.
+// state ? 어플리케이션에서 바뀌는 data
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (state = 0) => {
+  /**
+   * = reducer
+   * 1. 데이터를 바꿔줌 (modify state)
+   * 2. return하는 것은 우리 어플리케이션의 데이터임
+   * 3. 데이터를 바꿀 수 있는 유일한 함수 => 데이터가 한 곳에 있게 됨
+   */
+  return state;
 };
+const countStore = createStore(countModifier); // createStore는 Reducer가 필요함
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText(); // html에게 뭔가가 바뀌었다고 알려주는게 이상함! => 리덕스를 쓰는 이유
-};
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+console.log(countStore.getState()); // hello (reducer return)
